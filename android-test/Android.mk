0834a386f54bd0c8409985d4f40ecd0b2dd4c513
Kirby Kuehl <kkuehl@gmail.com>
2017-08-07 13:45:29 -0700

    Closer to working, gcc is deprecated.

--- android-test/Android.mk
+++ android-test/Android.mk
@@ -6,50 +6,50 @@ LOCAL_SRC_FILES := crash.c
 LOCAL_MODULE := crash
 
 # test afl-gcc/afl-g++ on ARM/Android
-# to build: TEST_GCC_ARM=true mm -B
+# to build: TEST_GCC_ARM=true mm
 ifeq ($(TEST_GCC_ARM), true)
 export AFL_CC := $(TARGET_CC)
 # expoet AFL_CXX := $(TARGET_CXX)
 export AFL_AS := $(TARGET_TOOLS_PREFIX)as
 LOCAL_CLANG := false
-LOCAL_CC := afl-gcc
+LOCAL_CC :=$(LOCAL_PATH)/../afl-gcc
 # LOCAL_CXX := afl-g++
 include $(BUILD_EXECUTABLE)
 endif
 
 # test afl-gcc/afl-g++ on host
-# to build: TEST_GCC_HOST=true mm -B
+# to build: TEST_GCC_HOST=true mm
 ifeq ($(TEST_GCC_HOST), true)
 export AFL_CC=$(HOST_CC)
 # export AFL_CXX=$(HOST_CXX)
 LOCAL_MULTILIB := 32
 export AFL_AS=$(HOST_TOOLCHAIN_PREFIX)as
 LOCAL_CLANG := false
-LOCAL_CC := afl-gcc
+LOCAL_CC :=$(LOCAL_PATH)/../afl-gcc
 # LOCAL_CXX := afl-g++
 include $(BUILD_HOST_EXECUTABLE)
 endif
 
 # test afl-clang/afl-clang++ on ARM/Android
-# to build: TEST_CLANG_ARM=true mm -B
+# to build:  AFL_USE_ARM=true TEST_CLANG_ARM=true mm 
 ifeq ($(TEST_CLANG_ARM), true)
 export AFL_CC=$(CLANG)
 # export AFL_CXX=$(CLANG_CXX)
 export AFL_AS=$(TARGET_TOOLS_PREFIX)as
 LOCAL_CLANG := true
-LOCAL_CC := afl-clang
+LOCAL_CC :=$(LOCAL_PATH)/../afl-clang
 # LOCAL_CC := afl-clang++
 include $(BUILD_EXECUTABLE)
 endif
 
 # test afl-clang/afl-clang++ on host
-# to build: TEST_CLANG_HOST=true mm -B
+# to build: TEST_CLANG_HOST=true mm
 ifeq ($(TEST_CLANG_HOST), true)
 export AFL_CC=$(CLANG)
 # export AFL_CXX=$(CLANG_CXX)
 export AFL_AS=$(HOST_TOOLCHAIN_PREFIX)as
 LOCAL_CLANG := true
-LOCAL_CC := afl-clang
+LOCAL_CC := $(LOCAL_PATH)/../afl-clang
 # LOCAL_CXX := afl-clang++
 include $(BUILD_HOST_EXECUTABLE)
 endif
